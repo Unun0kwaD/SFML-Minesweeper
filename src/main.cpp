2c82d65078cf9ae0189cb57f9f6a04000a22b0a6
Paweł Marczewski <pawel.mar.2001@gmail.com>
2024-05-13 13:17:09 +0200

    music added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 #include "minefield.h"
 #include "constants.h"
@@ -9,6 +10,14 @@ int main()
     window.setVerticalSyncEnabled(true);
     sf::Clock clock;
     Minefield minefield;
+
+    sf::Music music;
+    if (!music.openFromFile("../../audio/alex-productions-cyberpunk-computer-game-idra.mp3"))
+        return EXIT_FAILURE;
+
+    // Play the music
+    music.play();
+
     while (window.isOpen())
     {
         sf::Time elapsed = clock.getElapsedTime();
@@ -36,7 +45,7 @@ int main()
         }
         sf::Vector2i localPosition = sf::Mouse::getPosition(window);
         // std::cout<<localPosition.x<<" "<<localPosition.y<<std::endl;
-        minefield.highlight(localPosition.x/ CELL_SIZE,localPosition.y/ CELL_SIZE);
+        minefield.highlight(localPosition.x / CELL_SIZE, localPosition.y / CELL_SIZE);
 
         window.clear(sf::Color::White);
         minefield.Draw(window);
